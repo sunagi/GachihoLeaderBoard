f30ccfe7de947f9aac5098dacd03471c35467819
suna <akaractionend@icloud.com>
2023-07-30 14:40:35 +0900

    VLCNP

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import VLALanking from "./pages/VLA";
+import VLCNPLanking from "./pages/VLCNP";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path={`/`} element={<Home />} />
         <Route path={`/VLA`} element={<VLALanking />} />
+        <Route path={`/VLCNP`} element={<VLCNPLanking />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import '../App.css'
 import VLA from '../pictures/VLA.png'
+import VLCNP from '../pictures/VLCNP.png'
 import Card from '../components/Card'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <h1 style={{ "color": "#00CCFF", "fontFamily": "-apple-system,BlinkMacSystemFont,'Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue',sans-serif" }}>Gachiho Leader Board</h1>
       <Card image={VLA} title="VeryLongAnimals" link='/VLA' style={{ marginBottom: '20px' }} />
+      <Card image={VLCNP} title="VeryLongCNP" link='/VLCNP' style={{ marginBottom: '20px' }} />
       <hr
         style={{
           background: "#FFFFFF",
