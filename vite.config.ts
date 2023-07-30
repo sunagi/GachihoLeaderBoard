0c033239daaf47532eab9ef6ad966614d885fc34
suna <akaractionend@icloud.com>
2023-07-30 09:26:48 +0900

    VLA

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES
-    ? 'docs/'
-    : './',
+  base: process.env.GITHUB_PAGES ? 'docs/' : '/',
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000
-  },
+  server: process.env.NODE_ENV === 'development' ? { host: '0.0.0.0', port: 3000 } : {},
 })
